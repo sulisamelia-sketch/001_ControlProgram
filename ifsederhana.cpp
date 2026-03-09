3551427b55a38e2dd9d9117cbfb2e11e46739a5d
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-09 11:27:07 +0700

    Menambahkan library ke dalam project

--- ifsederhana.cpp
+++ ifsederhana.cpp
@@ -8,10 +8,6 @@ int main()
     srand(time(0));
     x = rand() % 10;
 
-    if (x == 5)
-    {
-        x = x + 1;
-        cout << "Nilai x Sudah dikoreksi menjadi : " << x << endl;
-    }
-cout << "Bilangan yang diberikan = " << x;
 }
+
+    
